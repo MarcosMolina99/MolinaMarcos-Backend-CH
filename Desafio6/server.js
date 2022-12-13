6bbbccafd4c9f8a8aaeeafe572c17124c4f9de0d
Marcos Molina <marcosgmolina998@gmail.com>
2022-12-13 15:36:29 -0300

    Cambios websocket

--- Desafio6/server.js
+++ Desafio6/server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api/productos", routerProductos);
@@ -28,35 +28,38 @@ app.engine(
     engine({
         extname: ".hbs",
         defaultLayout: join(__dirname, "public/layouts/main.hbs"),
-        layoutsDir: join(__dirname, "public/form.hbs"),
-        partialsDir: join(__dirname, "public/showProduct.hbs"),
+        layoutsDir: join(__dirname, "./public/"),
+        partialsDir: join(__dirname, "./public/partials"),
     })
 )
 
 app.set("view engine", "hbs");
 
-app.set("views", join(__dirname, "public/views"));
+app.set("views", join(__dirname, "public"));
 app.use(express.static("public"));
 
-app.listen(8080, () =>{
+
+
+let expressServer =app.listen(8080, () =>{
     console.log("Servidor en puerto 8080");
 });
-
-
 const io= new IOServer(expressServer);
 
-socket.emit("server:product", products);
+io.on("connection", (socket) =>{
+    socket.emit("server:product", products);
 
-socket.on("product: info", (productInfo)=>{
-    products.push(productInfo);
-    io.emit("server:products", products);
+    socket.on("product: info", (productInfo)=>{
+        products.push(productInfo);
+        io.emit("server:products", products);
+    })
+
+    socket.emit("server:message", messages);
+    socket.on("chat:messageInfo", (messageInfo) => {
+        messages.push(messageInfo);
+
+        fs.writeFileSync("./chat.txt", JSON.stringify(messages));
+
+        io.emit("server:message", messages);
+    });
 })
 
-socket.emit("server:message", messages);
-socket.on("chat:messageInfo", (messageInfo) => {
-  messages.push(messageInfo);
-
-  fs.writeFileSync("./chat.txt", JSON.stringify(messages));
-
-  io.emit("server:message", messages);
-});
